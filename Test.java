a31dc9d3cab7cc34d7d9c8bb97f24c4a669bfb07
dimasrehannabiel <dimasrehannabiel@gmail.com>
2019-05-08 16:09:39 +0700

    Update 1.0
    Done with the back and forth interface

--- Test.java
+++ Test.java
@@ -16,12 +16,14 @@ public class Test {
 		barang[5] = new Elektronik("TV", "SONY", "SY005", "Kuning", 1750000, 4, 100);
 		barang[6] = new Elektronik("TV", "LG", "LG006", "Hijau", 1250000, 2, 110);
 		Scanner scan = new Scanner(System.in);
+		//////////////
 		
+		//1.1 Tampilkan menu pertama
 		for(int a = 0; a == 0;)//Pengulangan agar tetap di menu pertama
 		{
-			//1.1 Tampilkan menu pertama
 			menu.TampilanPertama();
 			int pilihKategori = scan.nextInt();
+			scan.nextLine();
 			System.out.println("==========================================");
 			System.out.println();
 			System.out.println();
@@ -29,9 +31,9 @@ public class Test {
 			//2.1 Cek variabel pilihKategori
 			if(pilihKategori == 1 || pilihKategori == 2 || pilihKategori == 3) //Jika variabel [pilihKategori] valid
 			{
+				//2.2 Tampilkan menu kedua
 				for(int b = 0; b ==0;)//Pengulangan agar tetap di menu kedua
 				{
-					//2.2 Tampilkan menu kedua
 					menu.TampilanKedua(pilihKategori);
 					if(pilihKategori == 1)//2.3 Cek variabel pilih kategori agar list menyesuaikan
 					{
@@ -62,15 +64,16 @@ public class Test {
 						System.out.print("Apa pilihan anda? [ Masukkan Angka ] : ");
 					}
 					int pilihModel = scan.nextInt();
+					scan.nextLine();
 					System.out.println("==========================================");
 					System.out.println();
 					System.out.println();
-					////////////
+			/////////////////////////////////
 					
-					
+					//3.1 Lakukan pengecekan untuk variabel [pilihModel]
 					if(pilihModel == 1 || pilihModel == 2) //Jika variabel [pilihModel] valid
 					{
-						//3.1 Tampilkan detail barang berdasarkan variabel [pilihKategori] dan variabel [pilihModel]
+						//3.2 Tampilkan detail barang berdasarkan variabel [pilihKategori] dan variabel [pilihModel]
 						for(int c = 0; c == 0;) //Pengulangan agar tetap di menu detail barang
 						{
 							if(pilihKategori == 1) //[Mesin Cuci]
@@ -111,11 +114,102 @@ public class Test {
 							System.out.println("-- 2. Tidak ");
 							System.out.print("Apa pilihan anda? [ Masukkan Angka ] : ");
 							int pilihBeli = scan.nextInt();
+							scan.nextLine();
 							System.out.println("==========================================");
 							System.out.println();
 							System.out.println();
-							//////////
+					//////////////////////
+							
+							//4.1 Lakukan pengecekan untuk variabel [pilihBeli]
+							if(pilihBeli == 1 || pilihBeli == 2)//Jika variabel [pilihBeli] valid
+							{
+								//4.2 Tampilkan formulir pelanggan jika memilih untuk beli
+								if(pilihBeli == 1)
+								{
+									for(int d = 0; d == 0;) //Pengulangan agar tetap di form pelanggan
+									{
+										menu.TampilanForm();
+										
+										System.out.print("Nama Lengkap    : ");
+										String namaPembeli = scan.nextLine();	
+							
+										System.out.print("Alamat          : ");
+										String alamatPembeli = scan.nextLine();
+										
+										System.out.print("Email           : ");
+										String emailPembeli = scan.nextLine();
+										
+										System.out.print("Nomor Telephone : ");
+										String notelpPembeli = scan.nextLine();
+										
+										//4.3 Menginisialisasikan data pembeli
+										Pelanggan pembeli = new Pelanggan(namaPembeli, alamatPembeli, emailPembeli, notelpPembeli);
+										System.out.println("==========================================");
+										System.out.println();
+										System.out.println();
+										
+										if(namaPembeli.equals("") || alamatPembeli.equals("") || emailPembeli.equals("") || notelpPembeli.equals(""))
+										{
+											System.out.println("==========================================");
+											System.out.println("Terdapat form yang belum diisi atau tidak valid.");
+											System.out.println("Silahkan isi ulang form kembali dengan benar.");
+										}
+										else 
+										{
+											//4.3 Penyelksian kategori dan model barang
+											if(pilihKategori == 1 && pilihModel == 1) // [Mesin Cuci] dan [SN001]
+											{
+												struk.TampilanDetail(barang[1], pembeli);
+												System.out.println("==========================================");
+											}
+											else if(pilihKategori == 1 && pilihModel == 2) // [Mesin Cuci] dan [SP002]
+											{
+												struk.TampilanDetail(barang[2], pembeli);
+												System.out.println("==========================================");
+											}
+											else if(pilihKategori == 2 && pilihModel == 1) // [Kulkas] dan [PA003]
+											{
+												struk.TampilanDetail(barang[3], pembeli);
+												System.out.println("==========================================");
+											}
+											else if(pilihKategori == 2 && pilihModel == 2) // [Kulkas] dan [PN004]
+											{
+												struk.TampilanDetail(barang[4], pembeli);
+												System.out.println("==========================================");
+											}
+											else if(pilihKategori == 3 && pilihModel == 1) // [TV] dan [SY005]
+											{
+												struk.TampilanDetail(barang[5], pembeli);
+												System.out.println("==========================================");
+											}
+											else if(pilihKategori == 3 && pilihModel == 2) // [TV] dan [LG006]
+											{
+												struk.TampilanDetail(barang[6], pembeli);
+												System.out.println("==========================================");
+											}
+											break; //Menghentikan loop dari form pelanggan
+										}
+									}
+								}
+								else if(pilihBeli == 2)
+								{
+									System.out.println("==========================================");
+									System.out.println("Terima kasih telah menggunakan aplikasi kami.");
+									System.out.println("Silahkan melihat barang-barang kami kembali.");
+									break; //Menghentikan loop dari menu detail barang
+								}
+								break; //Menghentikan loop dari menu detail barang
+							}
+							else if(pilihBeli < 1 || pilihBeli > 2) //Jika variabel [pilihBeli] tidak valid
+							{
+								System.out.println("==========================================");
+								System.out.println("Maaf, kode angka yang dimasukkan tidak valid!");
+								System.out.println("Silahkan masukkan kode angka yang valid kembali.");
+							}
+							///////////////
+							
 						}
+						break; //Menghentikan loop dari menu kedua
 					}
 					else if(pilihModel < 1 || pilihModel > 2)//Jika variabel [pilihModel] tidak valid
 					{
@@ -123,7 +217,8 @@ public class Test {
 						System.out.println("Maaf, kode angka yang dimasukkan tidak valid!");
 						System.out.println("Silahkan masukkan kode angka yang valid kembali.");
 					}
-				}
+				}		
+				break; //Menghentikan loop dari menu pertama
 			}
 			else if(pilihKategori < 1 || pilihKategori > 3)//Jika variabel [pilihKategori] tidak valid
 			{
@@ -133,5 +228,7 @@ public class Test {
 			}
 			////////
 		}
+
+		/////////////////////
 	}
 }
